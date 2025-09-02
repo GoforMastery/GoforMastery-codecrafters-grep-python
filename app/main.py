fb1dac2ff90f77c6c048268bfb8747d74d057810
GoforMastery <balajireset744@gmail.com>
2025-09-02 12:20:49 +0530

    may be done.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
 import sys
 
+
 # import pyparsing - available if you need it!
 # import lark - available if you need it!
+def isDigit(input_char):
+    for ch in "1234567890":
+        if ch == input_char:
+            return True
+
+    return False
+
+
+def containsDigit(intput_line):
+    for chars in intput_line:
+        if isDigit(chars):
+            return True
+    return False
 
 
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
+    elif containsDigit(input_line):
+        exit(0)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
@@ -14,7 +30,10 @@ def match_pattern(input_line, pattern):
 def main():
     pattern = sys.argv[2]
     input_line = sys.stdin.read()
-
+    # print("checking")
+    # print(sys.argv[0])
+    # print(sys.argv[1])
+    # print(sys.argv[2])
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
         exit(1)
