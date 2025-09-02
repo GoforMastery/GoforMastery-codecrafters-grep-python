fc3465cb8b1bba42153733d948be12e6af9953ae
GoforMastery <balajireset744@gmail.com>
2025-09-02 17:07:12 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,18 +11,64 @@ def isDigit(input_char):
     return False
 
 
-def containsDigit(intput_line):
-    for chars in intput_line:
+def isAlpha(input_char):
+    for ch in "abcdefghijklmnopqrstuvwxyz":
+        if ch == input_char:
+            return True
+    for ch in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        if ch == input_char:
+            return True
+    return False
+
+
+def isUnderScore(input_char):
+    return input_char == "_"
+
+
+def containsDigit(input_line):
+    for chars in input_line:
         if isDigit(chars):
             return True
     return False
 
 
+def containsAlpha(input_line):
+    for chars in input_line:
+        if isAlpha(chars):
+            return True
+    return False
+
+
+def containsUnderScore(input_line):
+    for chars in input_line:
+        if chars == "_":
+            return True
+    return False
+
+
+def validChar(input_char):
+    return isDigit(input_char) or isAlpha(input_char) or isUnderScore(input_char)
+
+
+def validWord(input_line):
+    for chars in input_line:
+        if validChar(chars):
+            return True
+    return False
+
+
+# why my code is not working i dont know!
+
+import sys
+
+
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
-    elif containsDigit(input_line):
-        exit(0)
+    elif pattern == r"\d":
+        return containsDigit(input_line)
+    elif pattern == r"\w":
+        return validWord(input_line)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
@@ -30,18 +76,13 @@ def match_pattern(input_line, pattern):
 def main():
     pattern = sys.argv[2]
     input_line = sys.stdin.read()
-    # print("checking")
-    # print(sys.argv[0])
-    # print(sys.argv[1])
-    # print(sys.argv[2])
+
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
         exit(1)
 
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
+    print("Logs appear here!", file=sys.stderr)
 
-    # Uncomment this block to pass the first stage
     if match_pattern(input_line, pattern):
         exit(0)
     else:
