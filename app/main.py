f3a2ea801ec5f3de5d195fba334cc248cbee2d32
GoforMastery <balajireset744@gmail.com>
2025-09-06 11:06:56 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -91,9 +91,101 @@ def checkArrow(pattern):
     return pattern[0] == "[" and pattern[length - 1] == "]" and pattern[1] == "^"
 
 
+def containsThis(pattern):
+    return "\\w" in pattern or "\\d" in pattern
+
+
+def findthem(input_line, pattern):
+    i = 0
+    j = 0
+    ps = len(pattern)
+    while j < len(input_line):
+        if i < ps - 1 and pattern[i] == "\\" and pattern[i + 1] == "d":
+            if input_line[j].isdigit():
+                i += 2
+                if i == ps:
+                    return True
+            else:
+                i = 0
+        elif i < ps - 1 and pattern[i] == "\\" and pattern[i + 1] == "w":
+            if input_line[j].isalpha():
+                i += 2
+                if i == ps:
+                    return True
+            else:
+                i = 0
+        elif i < ps and input_line[j] == " " and pattern[i] == " ":
+            i += 1
+            if i == ps:
+                return True
+        elif i < ps and input_line[j] == pattern[i]:
+            i += 1
+            if i == ps:
+                return True
+        else:
+            i = 0
+
+        j += 1
+
+    return False
+
+
+def findThis(input_line, pattern):
+    # i feel i can use two pointers and sliding window
+    umap = {}
+    nmap = {}
+    for ch in pattern:
+        if containsAlpha(ch):
+            umap[r"\w"] = umap.get(r"\w", 0) + 1
+        elif containsDigit(ch):
+            umap[r"\d"] = umap.get(r"\d", 0) + 1
+        elif ch == " ":
+            umap[" "] = umap.get(" ", 0) + 1
+    i = 0
+    j = 0
+
+    while j < len(input_line):
+        if containsAlpha(input_line[j]):
+            nmap[r"\w"] = nmap.get(r"\w", 0) + 1
+        elif containsDigit(input_line[j]):
+            nmap[r"\d"] = nmap.get(r"\d", 0) + 1
+        elif input_line[j] == " ":
+            nmap[" "] = nmap.get(" ", 0) + 1
+
+        if j - i + 1 < len(pattern):
+            j = j + 1
+        elif j - i + 1 == len(pattern):
+            if umap == nmap:
+                k = i
+                p = 0
+                q = k + len(pattern)
+                while k < q:
+                    check = True
+                    if input_line[k] != pattern[p]:
+                        check = False
+                        break
+                    k = k + 1
+                    p = p + 1
+                if check == True:
+                    return True
+            if containsAlpha(input_line[i]):
+                nmap[r"\w"] -= 1
+            elif containsDigit(input_line[i]):
+                nmap[r"\d"] -= 1
+            elif input_line[i] == " ":
+                nmap[" "] -= 1
+
+            i = i + 1
+            j = j + 1
+
+    return False
+
+
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
+    elif findThis(input_line, pattern):
+        return findThis(input_line, pattern)
     elif pattern == r"\d":
         return containsDigit(input_line)
     elif pattern == r"\w":
@@ -102,6 +194,8 @@ def match_pattern(input_line, pattern):
         return positiveCharGroup(input_line, pattern)
     elif checkArrow(pattern):
         return negativeCharGroup(input_line, pattern)
+    elif containsThis(pattern):
+        return findthem(input_line, pattern)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
